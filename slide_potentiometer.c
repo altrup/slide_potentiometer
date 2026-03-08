1486c8b82db98ec16e4e9e7c6413b696c7d195db
altrup <51763643+altrup@users.noreply.github.com>
2026-03-07 20:44:10 -0500

    Read adc pins

--- slide_potentiometer.c
+++ slide_potentiometer.c
@@ -1,13 +1,25 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
-
+#include "hardware/adc.h"
 
 int main()
 {
     stdio_init_all();
 
+    adc_init();
+    adc_gpio_init(26); // ADC0
+    adc_gpio_init(27); // ADC1
+
     while (true) {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
+        adc_select_input(0);
+        uint16_t adc0 = adc_read();
+
+        adc_select_input(1);
+        uint16_t adc1 = adc_read();
+
+        uint16_t adc_avg = (adc0 + adc1) / 2;
+
+        printf("ADC: %u\n", adc_avg);
+        sleep_ms(100);
     }
 }

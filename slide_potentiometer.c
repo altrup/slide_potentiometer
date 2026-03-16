f5fac788a3cd42ae748285604ae220af7641084b
altrup <51763643+altrup@users.noreply.github.com>
2026-03-16 16:58:26 -0400

    remove all averages

--- slide_potentiometer.c
+++ slide_potentiometer.c
@@ -3,8 +3,7 @@
 #include "pico/stdlib.h"
 #include "hardware/adc.h"
 
-#define WINDOW_SIZE 100
-#define SLEEP_DUR 20
+#define SLEEP_DUR 100
 #define DEGREE 10
 
 int main()
@@ -13,12 +12,6 @@ int main()
 
     adc_init();
     adc_gpio_init(26); // ADC0
-    adc_gpio_init(27); // ADC1
-
-    uint16_t window[WINDOW_SIZE] = {0};
-    uint32_t window_sum = 0;
-    int window_index = 0;
-    int samples_collected = 0;
 
     double coefficients[DEGREE + 1] = {
         -1.57070636199206, 
@@ -37,26 +30,12 @@ int main()
 
     while (true) {
         adc_select_input(0);
-        uint16_t adc0 = adc_read();
+        uint16_t adc = adc_read();
 
-        adc_select_input(1);
-        uint16_t adc1 = adc_read();
-
-        uint16_t adc_avg = (adc0 + adc1) / 2;
-        window_sum -= window[window_index];
-        window[window_index] = adc_avg;
-        window_sum += adc_avg;
-
-        window_index = (window_index + 1) % WINDOW_SIZE;
-        if (samples_collected < WINDOW_SIZE)
-            samples_collected++;
-
-        uint16_t rolling_avg = window_sum / samples_collected;
-
-        printf("ADC Rolling Avg: %u\n", rolling_avg);
+        printf("ADC: %u\n", adc);
         double final_mm = 0;
         for (int i = 0; i <= DEGREE; i ++) {
-            final_mm += coefficients[i] * pow(rolling_avg, i) / pow(10, -exponents[i]);
+            final_mm += coefficients[i] * pow(adc, i) / pow(10, -exponents[i]);
         }
         printf("FINAL MEASUREMENT (MM): %f\n", final_mm);
         sleep_ms(SLEEP_DUR);
